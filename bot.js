d21828c86f34a5efc6155443de68ea761eb7e033
AbdouDz09 <45575757+AbdouDz09@users.noreply.github.com>
2018-12-16 11:46:39 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1231,7 +1231,7 @@ client.on('guildMemberAdd', member => {
     .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
     .setColor('RANDOM')
     .setImage('https://cdn.discordapp.com/attachments/521419225651347464/521796154044710923/---3.gif')
-var channel = member.guild.channels.find('name', '✗welcome')
+var channel = member.guild.channels.find('name', '✗𝐖𝐞𝐥𝐜𝐨𝐦𝐞')
 if (!channel) return;
 channel.send({embed : embed});
 });
